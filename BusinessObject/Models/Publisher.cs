3d75b05ba60e169248bd254ae848d9eb7606b10a
Trần Ngọc Hiếu <hieutnhe15199@fpt.edu.vn>
2023-06-19 21:51:46 +0700

    update

---
+++ BusinessObject/Models/Publisher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject.Models
+{
+    public partial class Publisher
+    {
+        public Publisher()
+        {
+            Books = new HashSet<Book>();
+            Users = new HashSet<User>();
+        }
+
+        public string PubId { get; set; } = null!;
+        public string PublisherName { get; set; } = null!;
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
